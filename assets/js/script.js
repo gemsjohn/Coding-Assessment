dc42128527108760db196a7c5af77353da6882d7
JRM <johnrhmartin@gmail.com>
2022-01-22 21:06:25 -0500

    initial setup. created HTML, CSS, and JS files. Add basic strucutre to each file.

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Get reference to the #begin-assessment element
+var generateBtn = document.querySelector("#begin-assessment");
+
+// Start Assessment
+function startAssessment() {
+    console.log("Start Assessment button pressed.")
+}
+
+// Add even listener to Start Assessment button
+generateBtn.addEventListener("click", startAssessment);
